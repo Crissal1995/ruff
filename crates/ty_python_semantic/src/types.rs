7ba407721add8f22c82b36b0514590803011b806
Charlie Marsh <charlie.r.marsh@gmail.com>
2026-01-14 13:59:49 -0500

    [ty] Add support for dynamic dataclasses via `make_dataclass`

--- crates/ty_python_semantic/src/types.rs
+++ crates/ty_python_semantic/src/types.rs
@@ -5075,6 +5075,10 @@ impl<'db> Type<'db> {
                     invalid_expressions: smallvec_inline![InvalidTypeExpression::NamedTupleSpec],
                     fallback_type: Type::unknown(),
                 }),
+                KnownInstanceType::DataclassSpec(_) => Err(InvalidTypeExpressionError {
+                    invalid_expressions: smallvec_inline![InvalidTypeExpression::DataclassSpec],
+                    fallback_type: Type::unknown(),
+                }),
                 KnownInstanceType::UnionType(instance) => {
                     // Cloning here is cheap if the result is a `Type` (which is `Copy`). It's more
                     // expensive if there are errors.
@@ -5778,6 +5782,7 @@ impl<'db> Type<'db> {
                 | KnownInstanceType::Literal(_)
                 | KnownInstanceType::LiteralStringAlias(_)
                 | KnownInstanceType::NamedTupleSpec(_)
+                | KnownInstanceType::DataclassSpec(_)
                 | KnownInstanceType::NewType(_) => {
                     // TODO: For some of these, we may need to try to find legacy typevars in inner types.
                 }
@@ -6706,6 +6711,8 @@ enum InvalidTypeExpression<'db> {
     Specialization,
     /// Same for `NamedTupleSpec`
     NamedTupleSpec,
+    /// Same for `DataclassSpec`
+    DataclassSpec,
     /// Same for `typing.TypedDict`
     TypedDict,
     /// Same for `typing.TypeAlias`, anywhere except for as the sole annotation on an annotated
@@ -6767,6 +6774,9 @@ impl<'db> InvalidTypeExpression<'db> {
                     ),
                     InvalidTypeExpression::NamedTupleSpec => {
                         f.write_str("`NamedTupleSpec` is not allowed in type expressions")
+                    }
+                    InvalidTypeExpression::DataclassSpec => {
+                        f.write_str("`DataclassSpec` is not allowed in type expressions")
                     }
                     InvalidTypeExpression::TypedDict => f.write_str(
                         "The special form `typing.TypedDict` \
--- crates/ty_python_semantic/src/types/function.rs
+++ crates/ty_python_semantic/src/types/function.rs
@@ -1684,6 +1684,8 @@ pub enum KnownFunction {
     Dataclass,
     /// `dataclasses.field`
     Field,
+    /// `dataclasses.make_dataclass`
+    MakeDataclass,
 
     /// `functools.total_ordering`
     TotalOrdering,
@@ -1785,7 +1787,7 @@ impl KnownFunction {
             Self::AsyncContextManager => {
                 matches!(module, KnownModule::Contextlib)
             }
-            Self::Dataclass | Self::Field => {
+            Self::Dataclass | Self::Field | Self::MakeDataclass => {
                 matches!(module, KnownModule::Dataclasses)
             }
             Self::TotalOrdering => module.is_functools(),
@@ -2272,7 +2274,9 @@ pub(crate) mod tests {
 
                 KnownFunction::AsyncContextManager => KnownModule::Contextlib,
 
-                KnownFunction::Dataclass | KnownFunction::Field => KnownModule::Dataclasses,
+                KnownFunction::Dataclass | KnownFunction::Field | KnownFunction::MakeDataclass => {
+                    KnownModule::Dataclasses
+                }
 
                 KnownFunction::GetattrStatic => KnownModule::Inspect,
 
